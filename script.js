2f4f618cc257b40a3070c7d072a3bcd14ec16ac6
DWW1993 <daniel.w.walker1993@gmail.com>
2017-09-28 13:40:03 -0500

    finished product

--- script.js
+++ script.js
@@ -2,40 +2,40 @@ var friends = ["Jordan", "Reagan", "Will", "Corey", "Roxie"];
 
 document.addEventListener("DOMContentLoaded", function(){
     var friend = document.getElementsByClassName("friend");
-    var btn = document.createElement("BUTTON");
+    var btn = document.createElement("button");
     var btntxt = document.createTextNode("Sing!");
     btn.appendChild(btntxt);
     document.body.appendChild(btn);
-    var sing = document.getElementsByTagName("BUTTON")[0];
+    var sing = document.getElementsByTagName("button")[0];
+
     sing.addEventListener("click", function(){
         for (var f = 0; f < friends.length; f++) {
-            
-            console.log(friends[f] + ":");
+            var fdiv = document.createElement("div");
+            fdiv.className = "friend";
+            var fh3 = document.createElement("h3");
+            fdiv.appendChild(fh3);
+            var fname = document.createTextNode(friends[f]);
+            fh3.appendChild(fname);
+            document.body.appendChild(fdiv);
+            console.log(fdiv);
             for (var b = 99; b > 0; b--) {
                 if(b >= 3) {
-                    console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file");
+                    var par = document.createElement("p"); 
+                    fdiv.appendChild(par);
+                    var song = document.createTextNode(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file");
+                    par.appendChild(song);
                 } else if(b === 2) { 
-                    console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " line of code in the file");
+                    var par = document.createElement("p"); 
+                    fdiv.appendChild(par);
+                    var song = document.createTextNode(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " line of code in the file");
+                    par.appendChild(song);
                 } else {
-                    console.log(b + " line of code in the file, " + b + " line of code; " + friends[f] + " strikes one out, clears it all out, no more lines of code in the file");
+                    var par = document.createElement("p"); 
+                    fdiv.appendChild(par);
+                    var song = document.createTextNode(b + " line of code in the file, " + b + " line of code; " + friends[f] + " strikes one out, clears it all out, no more lines of code in the file");
+                    par.appendChild(song);
                 }
             } 
         }
     });
 });
-
-//var sing= document.getElementsByTagName("button");
-//sing.addEventListener("click", function(){
-   // for (var f = 0; f < friends.length; f++) {
-       // console.log(friends[f] + ":");
-       // for (var b = 99; b > 0; b--) {
-            //if(b >= 3) {
-          //      console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file");
-           // } else if(b === 2) { 
-          //      console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " line of code in the file");
-         //   } else {
-         //       console.log(b + " line of code in the file, " + b + " line of code; " + friends[f] + " strikes one out, clears it all out, no more lines of code in the file");
-       //    }
-      //  } 
-   // }
-//});
