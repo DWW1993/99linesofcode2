813680a9c0a6e54b7aa13a15fe572ba4446cdce9
DWW1993 <daniel.w.walker1993@gmail.com>
2017-09-28 11:33:01 -0500

    scaffolding that I'm about to break horribly

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 var friends = ["Jordan", "Reagan", "Will", "Corey", "Roxie"];
 
-for (var f = 0; f < friends.length; f++) {
-    console.log(friends[f] + ":");
-    for (var b = 99; b > 0; b--) {
-        if(b >= 3) {
-            console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file");
-        } else if(b === 2) { 
-            console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " line of code in the file");
-        } else {
-            console.log(b + " line of code in the file, " + b + " line of code; " + friends[f] + " strikes one out, clears it all out, no more lines of code in the file");
+document.addEventListener("DOMContentLoaded", function(){
+    var friend = document.getElementsByClassName("friend");
+    var btn = document.createElement("BUTTON");
+    var btntxt = document.createTextNode("Sing!");
+    btn.appendChild(btntxt);
+    document.body.appendChild(btn);
+    var sing = document.getElementsByTagName("BUTTON")[0];
+    sing.addEventListener("click", function(){
+        for (var f = 0; f < friends.length; f++) {
+            
+            console.log(friends[f] + ":");
+            for (var b = 99; b > 0; b--) {
+                if(b >= 3) {
+                    console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file");
+                } else if(b === 2) { 
+                    console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " line of code in the file");
+                } else {
+                    console.log(b + " line of code in the file, " + b + " line of code; " + friends[f] + " strikes one out, clears it all out, no more lines of code in the file");
+                }
+            } 
         }
-    } 
-}
+    });
+});
+
+//var sing= document.getElementsByTagName("button");
+//sing.addEventListener("click", function(){
+   // for (var f = 0; f < friends.length; f++) {
+       // console.log(friends[f] + ":");
+       // for (var b = 99; b > 0; b--) {
+            //if(b >= 3) {
+          //      console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file");
+           // } else if(b === 2) { 
+          //      console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (b - 1) + " line of code in the file");
+         //   } else {
+         //       console.log(b + " line of code in the file, " + b + " line of code; " + friends[f] + " strikes one out, clears it all out, no more lines of code in the file");
+       //    }
+      //  } 
+   // }
+//});
